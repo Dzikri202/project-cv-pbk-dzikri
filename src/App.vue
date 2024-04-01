736827daeeb567a675d288787c09ee58a15aa221
softech.Dzikri202 <softech.dzikrifitra23@gmail.com>
2024-04-01 23:35:39 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,80 @@
+ <template>
+  <div id="app">
+    <header>
+      <h1>Dzikri Fitra Handrian</h1>
+      <p>Software Engineer | Web Developer</p>
+    </header>
+
+    <main>
+      <section id="about">
+        <h2>Tentang Saya</h2>
+        <p>
+          Saya adalah seorang mahasiswa informatika yang bersemangat dalam mempelajari berbagai aspek teknologi dan pemrograman. Saya memiliki rasa ingin tahu yang tinggi dan selalu ingin mencari solusi kreatif untuk berbagai permasalahan. Saya termotivasi untuk menjadi seorang ahli informatika 
+          yang dapat memberikan kontribusi positif bagi kemajuan teknologi di Indonesia.
+        </p>
+        <p>
+          Saya adalah seorang yang bersemangat dalam mempelajari teknologi baru dan selalu ingin meningkatkan kemampuan saya. 
+          Saya juga senang bekerja sama dengan tim dan membantu orang lain.
+        </p>
+      </section>
+
+      <section id="skills">
+        <h2>Keahlian</h2>
+        <ul>
+          <li>Pemrograman: Java, Python, C/C++</li>
+          <li>Web Development: VueJS, ReactJS, Laravel, HTML, CSS, JavaScript</li>
+          <li>Mobile Development: Flutter</li>
+          <li>Database: MySQL, PostgreSQL</li>
+          <li>Cloud Computing: AWS, Azure</li>
+        </ul>
+      </section>
+
+      <section id="experience">
+        <h2>Pengalaman Kerja/magang</h2>
+        <ul>
+          <li>
+            <strong>ip cctv, teknisi cctv</strong>
+            <br>
+             CV Bintang cctv(2021)
+          </li>
+    
+        </ul>
+      </section>
+
+      <section id="education">
+        <h2>Pendidikan</h2>
+        <ul>
+          <li>
+            <strong>Sedang menempuh S1 Teknik Informatika</strong>
+            <br>
+            Universitas Islam Riau (2022-2029)
+          </li>
+        </ul>
+      </section>
+
+      <section id="contact">
+        <h2>Kontak</h2>
+        <ul>
+          <li>Email: dzikrifitrahandrian23@gmail.com</li>
+          <li>LinkedIn: linkedin.com/in/dzikrifitrahandrian</li>
+          <li>Github: github.com/dzikrifitrahandrian</li>
+        </ul>
+      </section>
+    </main>
+
+    <footer>
+      <p>&copy; 2023 Dzikri Fitra Handrian</p>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+};
+</script>
+
+<style>
+/* ... */
+</style>
+
